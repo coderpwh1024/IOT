f0410309bc6cf9b036272665b929f7f0df97ee0c
pengwenhao <pengwenhao@qnvip.cn>
2024-01-24 17:17:40 +0800

    fix:update JwtTokenUtil

--- IOT-Guide-JWT-Without-JPA/src/main/java/com/coderpwh/config/JwtTokenUtil.java
+++ IOT-Guide-JWT-Without-JPA/src/main/java/com/coderpwh/config/JwtTokenUtil.java
@@ -1,11 +1,13 @@
 package com.coderpwh.config;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.function.Function;
 
 
@@ -28,7 +30,53 @@ public class JwtTokenUtil implements Serializable {
      * @return
      */
     public String getUsernameFromToken(String token) {
-        return null;
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+
+    /**
+     * token开始使用时间
+     *
+     * @param token
+     * @return
+     */
+    public Date getIssuedAtDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getIssuedAt);
+    }
+
+
+    /**
+     * token过期时间
+     *
+     * @param token
+     * @return
+     */
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+
+    /***
+     * 通过token获取Claim
+     * @param token
+     * @param claimsResolver
+     * @param <T>
+     * @return
+     */
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    /**
+     * 通过token获取claims
+     *
+     * @param token
+     * @return
+     */
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
 
 
