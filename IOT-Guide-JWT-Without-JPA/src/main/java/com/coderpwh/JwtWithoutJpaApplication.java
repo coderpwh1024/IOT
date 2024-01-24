47f94a6199154f25f077e5438d7bc2b6c3b09b24
pengwenhao <pengwenhao@qnvip.cn>
2024-01-24 16:06:09 +0800

    feat:init IOT

---
+++ IOT-Guide-JWT-Without-JPA/src/main/java/com/coderpwh/JwtWithoutJpaApplication.java
@@ -0,0 +1,17 @@
+package com.coderpwh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author james mu
+ * @date 2020/9/7 17:59
+ */
+@SpringBootApplication()
+public class JwtWithoutJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtWithoutJpaApplication.class, args);
+    }
+}
