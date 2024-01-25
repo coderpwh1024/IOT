8c4604f53835d54fad738ff5c2f8baba65d456e9
pengwenhao <pengwenhao@qnvip.cn>
2024-01-25 13:40:06 +0800

    feat:init JwtUserDetailsServiceImpl

---
+++ IOT-Guide-JWT-Without-JPA/src/main/java/com/coderpwh/service/impl/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.coderpwh.service.impl;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+
+/**
+ * @author coderpwh
+ */
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("iot.technology".equals(username)){
+            return new User("iot.technology", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        }else{
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+
+
+}
