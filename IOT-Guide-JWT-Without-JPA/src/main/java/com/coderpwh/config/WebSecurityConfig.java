8cb15f1ca3c545fa0d01df7343c442452a233d8b
pengwenhao <pengwenhao@qnvip.cn>
2024-01-30 11:14:57 +0800

    feat:init WebSecurityConfig

---
+++ IOT-Guide-JWT-Without-JPA/src/main/java/com/coderpwh/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.coderpwh.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * @author coderpwh
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    private final UserDetailsService userDetailsService;
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+
+    /***
+     * 有参构造
+     * @param jwtAuthenticationEntryPoint
+     * @param userDetailsService
+     * @param jwtRequestFilter
+     */
+    public WebSecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                             UserDetailsService userDetailsService,
+                             JwtRequestFilter jwtRequestFilter) {
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.userDetailsService = userDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+
+}
