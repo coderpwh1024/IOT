6a73dc05a4cbd044fe730d8e348b2a7349494af6
coderpwh <pengwenhao@aliyun.com>
2024-01-31 14:33:18 +0800

    fix:update JwtWithoutJpaApplication

--- IOT-Guide-JWT-Without-JPA/src/main/java/com/coderpwh/JwtWithoutJpaApplication.java
+++ IOT-Guide-JWT-Without-JPA/src/main/java/com/coderpwh/JwtWithoutJpaApplication.java
@@ -4,11 +4,11 @@ package com.coderpwh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author james mu
- * @date 2020/9/7 17:59
+/***
+ *
+ *
  */
-@SpringBootApplication()
+@SpringBootApplication(scanBasePackages = {"com.coderpwh"})
 public class JwtWithoutJpaApplication {
 
     public static void main(String[] args) {
