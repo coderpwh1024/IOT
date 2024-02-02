2b9d4cba2c4f301f4134333d0926dd62165a815e
coderpwh <pengwenhao@aliyun.com>
2024-02-02 11:17:57 +0800

    feat:init entity

---
+++ IOT-Guide-JWT-Refresh/src/main/java/com/coderpwh/entity/User.java
@@ -0,0 +1,42 @@
+package com.coderpwh.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+
+/**
+ * @author
+ */
+@Data
+@TableName(value = "t_user")
+public class User implements Serializable {
+    /**
+     * 主键id
+     */
+    @Id
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 权限
+     */
+    private String role;
+
+    private static final long serialVersionUID = 1L;
+
+
+}
---
+++ IOT-Guide-JWT-Refresh/src/main/java/com/coderpwh/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.coderpwh.mapper;
+
+import com.coderpwh.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * UserMapper继承基类
+ * @author coderpwh
+ */
+public interface UserMapper extends BaseMapper  <User> {
+
+}
