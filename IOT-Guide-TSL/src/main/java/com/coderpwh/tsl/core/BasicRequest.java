b786a2acefa3248e9cb1e86f95022d1510a2c583
coderpwh <pengwenhao@aliyun.com>
2024-03-20 09:20:42 +0800

    feat:init core

---
+++ IOT-Guide-TSL/src/main/java/com/coderpwh/tsl/core/BasicRequest.java
@@ -0,0 +1,16 @@
+package com.coderpwh.tsl.core;
+
+/**
+ * @author coderpwh
+ */
+public class BasicRequest {
+
+
+    public static final Integer DEFAULT_REQUEST_ID = 0;
+
+    private final Integer requestId;
+
+    public BasicRequest(Integer requestId) {
+        this.requestId = requestId;
+    }
+}
