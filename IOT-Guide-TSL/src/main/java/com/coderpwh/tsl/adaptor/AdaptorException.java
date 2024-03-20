4289b7a3459f3665bd3867c0a341bed7eca814ea
coderpwh <pengwenhao@aliyun.com>
2024-03-20 10:07:57 +0800

    fix:update AdaptorException

---
+++ IOT-Guide-TSL/src/main/java/com/coderpwh/tsl/adaptor/AdaptorException.java
@@ -0,0 +1,22 @@
+package com.coderpwh.tsl.adaptor;
+
+/**
+ * @author coderpwh
+ */
+public class AdaptorException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public AdaptorException() {
+        super();
+    }
+
+    public AdaptorException(String cause) {
+        super(cause);
+    }
+
+    public AdaptorException(Exception cause) {
+        super(cause);
+    }
+
+}
